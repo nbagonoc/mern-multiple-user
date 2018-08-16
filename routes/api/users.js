eb3463bc103baa1a6355470834b39b4702285393
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-08-16 22:30:41 +0800

    improved error handling with .catch method to prevent server crashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,13 +15,17 @@ router.get(
   "/profile",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    User.findOne({ _id: req.user.id }, (err, user) => {
-      if (err) {
-        res.json({ success: false, message: "User not found" });
-      } else {
-        res.json({ success: true, user });
-      }
-    });
+    User.findOne({ _id: req.user.id })
+      .then(user => {
+        if (user) {
+          res.json({ success: true, user });
+        } else {
+          res.json({ success: false, message: "User not found" });
+        }
+      })
+      .catch(ex => {
+        return res.status(500).send("Something went wrong");
+      });
   }
 );
 
@@ -31,13 +35,17 @@ router.get(
   "/",
   [passport.authenticate("jwt", { session: false }), isAdmin],
   (req, res) => {
-    User.find({ role: "subscriber" }, (err, users) => {
-      if (err) {
-        res.json({ success: false, message: "Users not found" });
-      } else {
-        res.json({ success: true, users });
-      }
-    });
+    User.find({ role: "subscriber" })
+      .then(users => {
+        if (users) {
+          res.json({ success: true, users });
+        } else {
+          res.json({ success: false, message: "Users not found" });
+        }
+      })
+      .catch(ex => {
+        return res.status(500).send("Something went wrong");
+      });
   }
 );
 
@@ -47,13 +55,17 @@ router.get(
   "/show/:id",
   [passport.authenticate("jwt", { session: false }), isAdmin],
   (req, res) => {
-    User.findOne({ _id: req.params.id }, (err, user) => {
-      if (err) {
-        res.json({ success: false, message: "User not found" });
-      } else {
-        res.json({ success: true, user });
-      }
-    });
+    User.findOne({ _id: req.params.id })
+      .then(user => {
+        if (user) {
+          res.json({ success: true, user });
+        } else {
+          res.json({ success: false, message: "User not found" });
+        }
+      })
+      .catch(ex => {
+        return res.status(500).send("Something went wrong");
+      });
   }
 );
 
@@ -66,16 +78,32 @@ router.put(
     if (!req.body.name) {
       res.json({ success: false, msg: "name is required" });
     } else {
-      User.findOne({ _id: req.body._id }, (err, user) => {
-        user.name = req.body.name;
-        user.save(err => {
-          if (err) {
-            res.json({ success: false, message: err });
+      User.findOne({ _id: req.params.id })
+        .then(user => {
+          if (user) {
+            user.name = req.body.name;
+            user
+              .save()
+              .then(userUpdated => {
+                if (userUpdated) {
+                  res.json({ success: true, message: "User updated!" });
+                } else {
+                  res.json({
+                    success: false,
+                    message: "User was not updated. Please try again"
+                  });
+                }
+              })
+              .catch(ex => {
+                return res.status(500).send("Something went wrong");
+              });
           } else {
-            res.json({ success: true, message: "user updated!" });
+            res.json({ success: false, message: "User not found" });
           }
+        })
+        .catch(ex => {
+          return res.status(500).send("Something went wrong");
         });
-      });
     }
   }
 );
@@ -86,15 +114,28 @@ router.delete(
   "/delete/:id",
   [passport.authenticate("jwt", { session: false }), isAdmin],
   (req, res) => {
-    User.findOne({ _id: req.params.id }, (err, user) => {
-      user.remove(err => {
-        if (err) {
-          res.json({ success: false, message: err });
+    User.findOne({ _id: req.params.id })
+      .then(user => {
+        if (user) {
+          user
+            .remove()
+            .then(userDeleted => {
+              if (userDeleted) {
+                res.json({ success: true, message: "User deleted" });
+              } else {
+                res.json({ success: false, message: "User was not deleted" });
+              }
+            })
+            .catch(ex => {
+              return res.status(500).send("Something went wrong");
+            });
         } else {
-          res.json({ success: true, message: "blog post deleted" });
+          res.json({ success: false, message: "User not found" });
         }
+      })
+      .catch(ex => {
+        return res.status(500).send("Something went wrong");
       });
-    });
   }
 );
 
